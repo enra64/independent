2a5331157247744ec3012b081f93e99346218037
Arne Herdick <sujomear@arcor.de>
2014-11-01 12:40:35 +0100

    added advanced db management entries

--- Votenote/src/de/oerntec/votenote/DatabaseCreator.java
+++ Votenote/src/de/oerntec/votenote/DatabaseCreator.java
@@ -25,10 +25,10 @@ public class DatabaseCreator extends SQLiteOpenHelper {
     public static final String ENTRIES_MAX_VOTES="max_votierung";
     public static final String ENTRIES_MY_VOTES="my_votierung";
     
-    public static final String TABLE_ENTRIES="uebungen_eintraege";
+    public static final String TABLE_NAME_ENTRIES="uebungen_eintraege";
     
     private static final String CREATE_DATABASE_ENTRIES = 
-    		"create table "+TABLE_ENTRIES+"( "+ENTRIES_ID+" integer primary key," +
+    		"create table "+TABLE_NAME_ENTRIES+"( "+ENTRIES_ID+" integer primary key," +
     		ENTRIES_TYP_UEBUNG+" int not null, " +
     		ENTRIES_NUMMER_UEBUNG+" integer not null," +
     		ENTRIES_MAX_VOTES+" integer not null," +
@@ -39,10 +39,10 @@ public class DatabaseCreator extends SQLiteOpenHelper {
     public static final String GROUPS_MIN_VOTE="uebung_minvote";
     public static final String GROUPS_PRESENTATIONPOINTS="uebung_prespoints";
     
-    public static final String TABLE_GROUPS="uebungen_gruppen";
+    public static final String TABLE_NAME_GROUPS="uebungen_gruppen";
     
     private static final String CREATE_DATABASE_GROUPS = 
-    		"create table "+TABLE_GROUPS+"( "+GROUPS_ID+" integer primary key," +
+    		"create table "+TABLE_NAME_GROUPS+"( "+GROUPS_ID+" integer primary key," +
     		GROUPS_NAMEN+" string not null," +
     		GROUPS_MIN_VOTE+" integer DEFAULT 50,"+
     		GROUPS_PRESENTATIONPOINTS + " integer DEFAULT 0);";
@@ -54,7 +54,7 @@ public class DatabaseCreator extends SQLiteOpenHelper {
     // Method is called during creation of the database
     @Override
     public void onCreate(SQLiteDatabase database) {
-    	database.execSQL(CREATE_DATABASE_ENTRIES);        
+    	database.execSQL(CREATE_DATABASE_ENTRIES);
     	database.execSQL(CREATE_DATABASE_GROUPS);
     }
 
@@ -64,6 +64,22 @@ public class DatabaseCreator extends SQLiteOpenHelper {
         Log.w(DatabaseCreator.class.getName(),
                          "Upgrading database from version " + oldVersion + " to "
                          + newVersion + ", which will destroy all old data");
+        /*
+         * more advanced db management
+         */
+        //rename old db, create new one;
+        database.execSQL("ALTER TABLE "+TABLE_NAME_ENTRIES+" RENAME TO oldentries");
+    	database.execSQL(CREATE_DATABASE_ENTRIES);
+    	//copy values
+        database.execSQL("INSERT INTO "+TABLE_NAME_ENTRIES+" ("
+            	+ENTRIES_ID+", "+ENTRIES_TYP_UEBUNG+", "+ENTRIES_NUMMER_UEBUNG+", "+ENTRIES_MAX_VOTES+", "+ENTRIES_MY_VOTES+") "
+            			+ "SELECT "
+            	+ENTRIES_ID+", "+ENTRIES_TYP_UEBUNG+", "+ENTRIES_NUMMER_UEBUNG+", "+ENTRIES_MAX_VOTES+", "+ENTRIES_MY_VOTES+"  FROM oldentries");
+        //drop old one
+        database.execSQL("DROP TABLE IF EXISTS oldentries");
+        /*
+         * old db manager
+         */
         database.execSQL("DROP TABLE IF EXISTS uebungen_eintraege");
         database.execSQL("DROP TABLE IF EXISTS uebungen_gruppen");
         onCreate(database);
